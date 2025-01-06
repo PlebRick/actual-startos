ccf91348364e9c408e5bdf35aadc8f58e8776693
PlebRick <B1ackSwan@Protonmail.com>
2025-01-06 14:20:32 -0600

    cleanup

--- startos/versions/v24.12.0.1.ts
+++ startos/versions/v24.12.0.1.ts
@@ -1,14 +1,11 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 import { sdk } from '../sdk'
-import { setName } from '../actions/setName'
 
 export const v24_12_0_1 = VersionInfo.of({
   version: '24.12.0:1',
   releaseNotes: 'Dashboards, tracking budget, summary report, batch sync for simpleFin',
   migrations: {
-    up: async ({ effects }) => {
-      await sdk.action.requestOwn(effects, setName, 'critical')
-    },
+    up: async ({ effects }) => {},
     down: IMPOSSIBLE,
   },
 })
