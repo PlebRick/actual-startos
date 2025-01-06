00b5f27eb46d848c3ea007a30e5eda550fe8d981
PlebRick <B1ackSwan@Protonmail.com>
2025-01-06 14:07:53 -0600

    cleanup

--- startos/init.ts
+++ startos/init.ts
@@ -4,21 +4,9 @@ import { setDependencies } from './dependencies'
 import { setInterfaces } from './interfaces'
 import { versions } from './versions'
 import { actions } from './actions'
-import { getSecretPhrase } from './utils'
-import { yamlFile } from './file-models/config.yml'
 
 // **** Install ****
-const install = sdk.setupInstall(async ({ effects }) => {
-  const name = 'World'
-
-  await yamlFile.write({ name })
-
-  await sdk.store.setOwn(
-    effects,
-    sdk.StorePath.secretPhrase,
-    getSecretPhrase(name),
-  )
-})
+const install = sdk.setupInstall(async ({ effects }) => {})
 
 // **** Uninstall ****
 const uninstall = sdk.setupUninstall(async ({ effects }) => {})
